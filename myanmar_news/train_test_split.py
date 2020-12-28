d7e9137b57e957a6b0de7144b16b3f7ce5fe7ab7
HungLV <hunglv@fimo.edu.vn>
2020-12-28 17:12:46 +0700

    add dev set

--- myanmar_news/train_test_split.py
+++ myanmar_news/train_test_split.py
@@ -11,7 +11,10 @@ with open("topics.csv", encoding="utf-8") as f:
         X.append(row[0])
         y.append(row[1])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.125, random_state=1, stratify=y_train) # 0.125 * 0.8 = 0.2
+
+
 with open("train.txt", 'w', encoding="utf-8", newline='') as f:
     writer = csv.writer(f, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for text, category in zip(X_train, y_train):
@@ -20,4 +23,9 @@ with open("train.txt", 'w', encoding="utf-8", newline='') as f:
 with open("test.txt", 'w', encoding="utf-8", newline='') as f:
     writer = csv.writer(f, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for text, category in zip(X_test, y_test):
+        writer.writerow([text, category])
+
+with open("dev.txt", 'w', encoding="utf-8", newline='') as f:
+    writer = csv.writer(f, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for text, category in zip(X_val, y_val):
         writer.writerow([text, category])
